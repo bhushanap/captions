a4deed97d6b1f69ac461f35ce7f1918facae6047
bhushan <83635464+bhushanap@users.noreply.github.com>
2023-12-06 18:05:02 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,8 @@
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# Move to the parent directory of the script
+cd "$SCRIPT_DIR"/.. && \
+
 rm -rf caption && \
 python3 -m venv caption  && \
 source caption/bin/activate  && \
