7c3e65faa4bd095f6459b695955d2dbc7f6ab61f
bhushan <83635464+bhushanap@users.noreply.github.com>
2023-12-06 17:29:40 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 if [ -d "caption" ]; then
-    source caption/bin/activate && \
+    source caption/bin/activate
 else
-    python3 -m venv caption  && \
-    source caption/bin/activate && \
+    python3 -m venv caption && source caption/bin/activate
 fi
+
 python3 caption.py
