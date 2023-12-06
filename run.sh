7beb919b8b533ccf3673d6ca7fc8303121768472
bhushan <83635464+bhushanap@users.noreply.github.com>
2023-12-06 18:05:13 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,8 @@
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# Move to the parent directory of the script
+cd "$SCRIPT_DIR"/.. && \
+
 if [ -d "caption" ]; then
     source caption/bin/activate
 else
