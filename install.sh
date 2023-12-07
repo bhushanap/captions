16ab9f494ccbe1e6e1e50ee8c882b04baef65dc1
bhushan <83635464+bhushanap@users.noreply.github.com>
2023-12-06 22:04:26 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,24 @@
-deactivate && \
+# Check if virtual environment is active
+if [ -n "$VIRTUAL_ENV" ]; then
+    deactivate
+fi
+
+# Get the directory of the script
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Move to the parent directory of the script
 cd "$SCRIPT_DIR"/ && \
 
-rm -rf caption && \
-python3 -m venv caption  && \
-source caption/bin/activate  && \
-pip install pywhispercpp && \
-chmod +x run.sh && \
+# Remove existing "caption" directory
+rm -rf caption
+
+# Create and activate virtual environment
+python3 -m venv caption
+source caption/bin/activate
+
+# Install dependencies
+pip install pywhispercpp
+
+# Make run.sh executable and execute it
+chmod +x run.sh
 . run.sh
