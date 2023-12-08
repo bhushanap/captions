82fd9a3a57169669f35af0fc56b54c804f1fda2c
bhushan <83635464+bhushanap@users.noreply.github.com>
2023-12-07 22:18:12 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,7 +10,6 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "$SCRIPT_DIR"/ && \
 
 sudo snap install yq && \
-sudo snap install ffmpeg && \
 
 # Remove existing "caption" directory
 rm -rf caption && \
